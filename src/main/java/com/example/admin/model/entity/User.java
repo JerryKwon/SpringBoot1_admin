e6cfe60ab824cfc8c39262e50bcc185187b1f19e
JerryKwon <kwonyoung234@gmail.com>
2019-11-28 22:27:22 +0900

    스프링부트 admin 만들기 2일차

--- src/main/java/com/example/admin/model/entity/User.java
+++ src/main/java/com/example/admin/model/entity/User.java
@@ -25,11 +25,17 @@ public class User {
 	
 //	@Column(name="account")
 	private String account;
+	
 	private String email;
+	
 	private String phoneNumber;
+	
 	private LocalDateTime createdAt;
+	
 	private String createdBy;
+	
 	private LocalDateTime updatedAt;
+	
 	private String updatedBy;
 	
 }
